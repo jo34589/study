ca02cf17562358859bdd4a7b4d437d269b13042f
N_Naru <deadee31@gmail.com>
2021-11-11 11:07:20 +0900

    chapter07
    코어 데이터 수동 반입 실습

---
+++ appmaking_study/Chapter07-ManualCoreData/Chapter07-ManualCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter07-ManualCoreData
+//
+//  Created by 엔나루 on 2021/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
