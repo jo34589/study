5f7ff83b221dd65ead6acd9f6af73ef2ec0b64a3
N_Naru <deadee31@gmail.com>
2021-11-30 12:06:21 +0900

    정리, SwiftUI 시작

---
+++ SwiftUI 공부/developer.apple.com SwiftUI 튜토리얼/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 엔나루 on 2021/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Tuttle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+            ContentView()
+    }
+}
---
+++ SwiftUI 공부/developer.apple.com SwiftUI 튜토리얼/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 엔나루 on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
