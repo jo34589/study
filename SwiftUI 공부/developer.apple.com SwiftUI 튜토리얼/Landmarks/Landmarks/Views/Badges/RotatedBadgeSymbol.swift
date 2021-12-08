0e96494d6837ee7211740f701a47d03c533ea6e3
N_Naru <deadee31@gmail.com>
2021-12-08 22:50:26 +0900

    DarwingPathAndShapes

---
+++ SwiftUI 공부/developer.apple.com SwiftUI 튜토리얼/Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by 엔나루 on 2021/12/08.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 6))
+    }
+}
