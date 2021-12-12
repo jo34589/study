5736307de4eb0208c77e6f9831cec207fa2c7cee
N_Naru <deadee31@gmail.com>
2021-12-12 15:46:29 +0900

    ComposingComplexInterfaces

--- SwiftUI 공부/developer.apple.com SwiftUI 튜토리얼/Landmarks/Landmarks/Model/ModelData.swift
+++ SwiftUI 공부/developer.apple.com SwiftUI 튜토리얼/Landmarks/Landmarks/Model/ModelData.swift
@@ -13,6 +13,18 @@ final class ModelData: ObservableObject {
     //will never modify hike data after loading it
     //so no need to put @Published
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories : [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: {$0.category.rawValue}
+        )
+    }
+    
+    var features: [Landmark] {
+        landmarks.filter() {$0.isFeatured}
+    }
+    
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
---
+++ SwiftUI 공부/developer.apple.com SwiftUI 튜토리얼/Landmarks/Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,48 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by 엔나루 on 2021/12/12.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(3))
+            
+        )
+    }
+}
